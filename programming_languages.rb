1f79337865848310ecfcd1164e4be706a865c2fb
hao wang <ge_nah@hotmail.com>
2019-05-14 21:20:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |styles, data|
+    data.each do |language, types|
+      new_hash[language] = {}
+    end
+  end
+  new_hash.each do |key, value|
+    value[:style] = [] 
+  end
+  languages[:oo].each do |language, value|
+    new_hash.each do |lan, data|
+      if language == lan
+        data[:style] << :oo 
+      end
+    end
+  end
+  
+  languages[:functional].each do |language, value|
+    new_hash.each do |lan, data|
+      if language == lan
+        data[:style] << :functional 
+      end
+    end
+  end
+  new_hash
 end
+
